09fe6aebb04533a71fea7642336a60848b753198
James <108743432+JamesDracupWCU@users.noreply.github.com>
2023-06-15 19:30:26 -0400

    Update setup_control.sh

--- setup_control.sh
+++ setup_control.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+# generate passwordless SSH
+# may not need sudo
+sudo runuser -u jd926102 -- ssh-keygen -q -t rsa -f /users/jd926102/.ssh/id_rsa -N ''
+sudo runuser -u jd926102 -- cat /users/jd926102/.ssh/id_rsa.pub >> /users/jd926102/.ssh/authorized_keys
