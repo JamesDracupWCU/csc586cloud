82d95a79825d4d56c7a2dc9d3f091ccd68732d22
James <108743432+JamesDracupWCU@users.noreply.github.com>
2023-06-15 20:41:05 -0400

    Create setup_host.sh

---
+++ setup_host.sh
@@ -0,0 +1,14 @@
+#!/bin.bash
+
+apt update
+apt install -y nfs-common
+mkdir -p /var/nfs/keys
+
+while [ ! -f /var/nfs/keys/id_rsa ]; do
+  mount 192.168.1.1:/var/nfs/keys /var/nfs/keys
+  sleep 10
+ done
+ 
+ cp /var/nfs/keys/id_rsa* /users/jd926102/.ssh/
+ chown jd926102: /users/jd926102/.ssh/id_rsa*
+ runuser -u jd926102 -- cat /users/jd926102/.ssh/id_rsa.pub >> /users/jd926102/.ssh/authorized_keys
