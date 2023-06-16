b0048bfc0e88e0b7a95582b552c54d7869dcf593
James <108743432+JamesDracupWCU@users.noreply.github.com>
2023-06-15 21:39:44 -0400

    Update setup_control.sh

--- setup_control.sh
+++ setup_control.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# generate passwordless SSH
-# may not need sudo
+# generate passwordless SSH# may not need sudo
 sudo runuser -u jd926102 -- ssh-keygen -q -t rsa -f /users/jd926102/.ssh/id_rsa -N ''
 sudo runuser -u jd926102 -- cat /users/jd926102/.ssh/id_rsa.pub >> /users/jd926102/.ssh/authorized_keys
 
@@ -13,4 +12,18 @@ sudo cp /users/jd926102/.ssh/id_rsa* /var/nfs/keys
 sudo chown nobody:nogroup /var/nfs/keys
 echo "/var/nfs/keys   192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 systemctl restart nfs-kernel-server
+
+# setup ansible
+apt-add-repository -y ppa:ansible/ansible
+apt update
+apt install -y ansible
+cp /local/repository/hosts /etc/ansible/
+
+# setup lamp
+git clone https://github.com/do-community/ansible-playbooks.git
+cd ansible-playbooks/lamp_ubuntu1804/
+cp /local/repository/default.yml vars/
+HOST=$(hostname -f)
+sed -i "s/HOSTNAME/$HOST/g" vars/default.yml
+ansible-playbook playbook.yml -l server1 -u jd926102
 
