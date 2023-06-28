ab521f630ebef409ae58d76db8baf8dc0041976c
James <108743432+JamesDracupWCU@users.noreply.github.com>
2023-06-28 13:05:04 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-Login="/var/log/auth.log"
-unauthorized_login="/var/webserver_log/unauthorized.log"
-failedAttempts=$(grep "Failed password" "$Login")
-
+login="/var/log/auth.log"
+failed=$(grep "sshd" "$login" | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
+for ip in $failed; do
+        country=$(curl -s "https://ipinfo.io/$ip/country" | tr -d '"')
+        echo "IP: $ip, Country: $country"
+        echo "IP: $ip, Country: $country" >> "/var/webserver_log/unauthorized.log"
+done
 #To edit Cron
 #crontab -e and then modify it for every 5 minutes it should execute the scan.sh /_/_/scan.sh
