3de5717b81670705cd08d514863818b554b054d6
James <108743432+JamesDracupWCU@users.noreply.github.com>
2023-06-27 21:06:36 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -2,3 +2,6 @@
 Login="/var/log/auth.log"
 unauthorized_login="/var/webserver_log/unauthorized.log"
 failedAttempts=$(grep "Failed password" "$Login")
+
+#To edit Cron
+#crontab -e and then modify it for every 5 minutes it should execute the scan.sh /_/_/scan.sh
