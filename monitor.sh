8338b31af6523e10a0308af31e498a9316421c32
James <108743432+JamesDracupWCU@users.noreply.github.com>
2023-06-30 07:50:45 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 log="/var/webserver_monitor/unauthorized.log"
-#Actual email should be placed here to be notified if new unauthorized attempts occur in the 5-minute intervals or not.
-admin_email="myemail@gmail.com"
+#Actual email to be notified every hour if new unauthorized attempts occur in the 5-minute intervals or not.
+admin_email="jamesdracup16@gmail.com"
 
 if [ -f "$log" ]; then
     current=$(cat location.txt || echo 0)
     new=$(tail -c +$((current + 1)) "$log")
 
     if [ -n "$new" ]; then
-        echo "$new" | mail -s "New Unauthorized/Failed Access Attempts Detected" "$admin_email"
+        echo "$new" | mail -s "New Unauthorized Access Detected:" "$admin_email"
         echo "$(wc -l < "$log")" > location.txt
     else
-        echo "No Unauthorized/Failed Access Attempts." | mail -s "No Unauthorized/Failed Access Attempts" "$admin_email"
+        echo "No unauthorized access." | mail -s "No unauthorized access" "$admin_email"
     fi
 else
-    echo "Log file not found: $log"
-fi
