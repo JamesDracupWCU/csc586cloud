4ecf8f899d94f4c78a1c9d94ffa05dd300c69e99
James <108743432+JamesDracupWCU@users.noreply.github.com>
2023-06-29 21:38:12 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 login="/var/log/auth.log"
-failed=$(grep "Invalid user" "$login" | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
-for ip in $failed; do
-#got rid of country after / tr -d
-        date=$(grep "$ip" "$login" | awk '{print  $1" "  $2" " $3" " }' | head -n 1 )
+failedip=$(grep "Invalid user" "$login" | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
+for ip in $failedip; do
+        date=$(grep "$ip" "$login" | awk '{print $1, $2, $3}' | head -n 1)
+   if ! grep -q "$ip" "/var/webserver_log/unauthorized.log"; then
+     if ! grep -q "$date" "/var/webserver_log/unauthorized.log"; then
         country=$(sudo geoiplookup $ip | awk -F ', ' '{print $2}')
- #  country=$(curl -s https://ipinfo.io/$ip/ | tr -d )
-        echo "IP: $ip, Country: $country,   Date: $date"
-        echo "IP: $ip, Country: $country,   Date: $date" >> "/var/webserver_log/unauthorized.log"
+        echo "IP: $ip, Country: $country, Date: $date"
+        echo "IP: $ip, Country: $country, Date: $date" >> "/var/webserver_log/unauthorized.log"
+     fi 
+   fi
 done
