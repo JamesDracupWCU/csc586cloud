662a4a7fde9a116c6e98fa9f2366ffe0d3b8ea9f
James <108743432+JamesDracupWCU@users.noreply.github.com>
2023-06-29 22:05:54 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -4,10 +4,8 @@ failedip=$(grep "Invalid user" "$login" | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}"
 for ip in $failedip; do
         date=$(grep "$ip" "$login" | awk '{print $1, $2, $3}' | head -n 1)
    if ! grep -q "$ip" "/var/webserver_log/unauthorized.log"; then
-     if ! grep -q "$date" "/var/webserver_log/unauthorized.log"; then
         country=$(sudo geoiplookup $ip | awk -F ', ' '{print $2}')
         echo "IP: $ip, Country: $country, Date: $date"
         echo "IP: $ip, Country: $country, Date: $date" >> "/var/webserver_log/unauthorized.log"
-     fi 
    fi
 done
