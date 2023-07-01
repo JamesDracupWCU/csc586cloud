6e7143b9565e6cfff6a641573be8ebab9f94b0c8
James <108743432+JamesDracupWCU@users.noreply.github.com>
2023-07-01 19:15:04 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,12 +3,12 @@
 log="/var/webserver_monitor/unauthorized.log"
 admin_email="jamesdracup16@gmail.com"
 previous_append=$(cat "/var/webserver_monitor/previous_append.txt" 2>/dev/null)
+
 current_append=$(cat "$log")
 
-if [ -s "$log" ] &&  [ "$current_append" != "$previous_append" ]; then
-    echo -e "New Unauthorized Access Attempts (Newest appended at bottom):\n\n" "$current_append" | mail -s "New Unauthorized Access" "$admin_email"
+if [ -s "$log" ] && [ "$current_append" != "$previous_append" ]; then
+    echo -e "New Unauthorized Access:\n\n$current_append" | mail -s "New Unauthorized Access" "$admin_email"
+    echo "$current_append" > "/var/webserver_monitor/previous_append.txt"
 else
-    echo "No Unauthorized Access" | mail -s "No Unauthorized Access" "$admin_email"
+    echo -e "No Unauthorized Access" | mail -s "No Unauthorized Access" "$admin_email"
 fi
-
-echo "$current_append" > "/var/webserver_monitor/previous_append.txt"
