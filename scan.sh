8939259040b6737c3b741fc635d35d4e19a5a189
James <108743432+JamesDracupWCU@users.noreply.github.com>
2023-07-01 19:13:33 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
- 2 login="/var/log/auth.log"
- 3 failedip=$(grep "Invalid user" "$login" | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
- 4 for ip in $failedip; do
- 5         date=$(grep "$ip" "$login" | awk '{print $1, $2,"'$(date +'%Y')'", $3}' | head -n 1)
- 6    if ! grep -q "$ip" "/var/webserver_log/unauthorized.log"; then
- 7         country=$(sudo geoiplookup $ip | awk -F ', ' '{print $2}')
- 8        printf "IP: %-19s Country: %-19s Date: %s\n" "$ip" "$country" "$date"
- 9        printf "IP: %-19s Country: %-19s Date: %s\n" "$ip" "$country" "$date" >> "/var/webserver_log/unauthorized.log"
-10    fi
-11 done
+  login="/var/log/auth.log"
+  failedip=$(grep "Invalid user" "$login" | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
+  for ip in $failedip; do
+          date=$(grep "$ip" "$login" | awk '{print $1, $2,"'$(date +'%Y')'", $3}' | head -n 1)
+     if ! grep -q "$ip" "/var/webserver_log/unauthorized.log"; then
+        if [[ $ip == "103.176.96.74" ]]; then
+            country="Indonesia"
+        elif [[ $ip == "103.149.28.105" ]]; then
+            country="Vietnam"
+        else
+            country=$(sudo geoiplookup $ip | awk -F ', ' '{print $2}')
+        fi
+            printf "IP: %-19s Country: %-19s Date: %s\n" "$ip" "$country" "$date"
+            printf "IP: %-19s Country: %-19s Date: %s\n" "$ip" "$country" "$date" >> "/var/webserver_log/unauthorized.log"
+     fi
+  done
